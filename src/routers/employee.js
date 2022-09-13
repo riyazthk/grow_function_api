5f9bf33592b735aea334a77da2b77e54cb59a2ff
riyazthk <er.riyaz2507@gmail.com>
2022-09-14 01:59:49 +0530

    add cors package

--- src/routers/employee.js
+++ src/routers/employee.js
@@ -26,9 +26,10 @@ router.post('/add_employee_details',async(req,res)=>{
 
 router.get('/get_employee_details',(req,res)=>{
     console.log('entry request');
-const {page,size,first_name,last_name,date_of_birth,date_enrolled}=req.query
+const {page,size,first_name,last_name,date_of_birth,date_enrolled}=req?.query
 let filter_payload={}
-
+var pages=page
+var sizes=size
 if(first_name) filter_payload={...filter_payload,first_name:first_name}
 
 if(last_name) filter_payload={...filter_payload,last_name:last_name}
@@ -37,16 +38,16 @@ if(date_of_birth)filter_payload={...filter_payload,date_of_birth:date_of_birth}
 
 if(date_enrolled) filter_payload={...filter_payload,date_enrolled:date_enrolled}
 
-if (!page) page = 1;
+if (!pages) pages = 1;
 
-if (!size) size = 3;
+if (!sizes) sizes = 3;
 
-    const limit = parseInt(size)
-    const skip = (parseInt(page) - 1) * parseInt(size)
+    const limit = parseInt(sizes)
+    const skip = (parseInt(pages) - 1) * parseInt(sizes)
 
     Employee.find(filter_payload).limit(limit).skip(skip).then(response=>{
 
-        res.send({status:200,success:true,parameters:response,page:parseInt(page)+1,size:parseInt(size)})
+        res.send({status:200,success:true,parameters:response,page:parseInt(pages)+1,size:parseInt(sizes)})
     }).catch(err=>{
         res.send({status:403,success:false,message:'Invalid'})
     })
